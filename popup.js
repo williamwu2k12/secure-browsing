a4a191eee0a3e86cbab843fc834efca0cc984e2a
William Wu <williamwu.2k12@gmail.com>
2014-03-01 14:51:04 -0800

    added storage permissions and store and get data

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
-console.log("figure out how to get links saved while in incognito (chrome.tabs.incognito");
+console.log("figure out how to get links saved while in incognito (chrome.tabs.incognito)");
+// StorageArea.set({"one": "please save this, thank you"}, function(){console.log});
+// result = StorageArea.get("one");
+// console.log(result);
+
+var StorageArea = chrome.storage.local;
+function store(key, value)
+{
+    StorageArea.set({key: value}, function(){console.log("value saved");});
+}
+
+function get(key)
+{
+    return StorageArea.get(key, function(){console.log("value received");});
+}
+
+store("one", "please save this, thank you");
+console.log(get("one"));
 
 // chrome.tabs.onCreated.addListener(function(tab)
 // {
