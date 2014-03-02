0fffa2d35d15c0d3690ce8f2daadeaba3f9738a6
William Wu <williamwu.2k12@gmail.com>
2014-03-01 16:42:34 -0800

    fix merge

--- popup.js
+++ popup.js
@@ -4,18 +4,48 @@ console.log("figure out how to get links saved while in incognito (chrome.tabs.i
 // console.log(result);
 
 var StorageArea = chrome.storage.local;
+
+var cipher = {};
+
 function store(key, value)
 {
-    StorageArea.set({key: value}, function(){console.log("value saved");});
+    var object = {};
+    var locked = encrypt(value);
+    object[key] = locked;
+    StorageArea.set(object);
 }
-
 function get(key)
 {
-    return StorageArea.get(key, function(){console.log("value received");});
+    StorageArea.get(key, function(object)
+    {
+        console.log(object[key]);
+    });
 }
 
-store("one", "please save this, thank you");
-console.log(get("one"));
+function encrypt(value)
+{
+    var secret = "";
+    secret.append()
+    return value;
+}
+
+function decrypt(value)
+{
+    return value;
+}
+StorageArea.clear(function(){});
+// store("one", "please save this, thank you");
+// get("one");
+
+
+
+
+
+
+
+
+
+
 
 // chrome.tabs.onCreated.addListener(function(tab)
 // {
