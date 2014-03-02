8b4c46e53ff631989fe2ad44df18ad99d899cc6a
William Wu <williamwu.2k12@gmail.com>
2014-03-01 19:15:55 -0800

    substitution cryptography

--- storage.js
+++ storage.js
@@ -1,11 +1,5 @@
-console.log("figure out how to get links saved while in incognito (chrome.tabs.incognito)");
-// StorageArea.set({"one": "please save this, thank you"}, function(){console.log});
-// result = StorageArea.get("one");
-// console.log(result);
-
 var StorageArea = chrome.storage.local;
-
-var cipher = {};
+var cipher = setupCipher();
 
 function store(key, value)
 {
@@ -14,6 +8,7 @@ function store(key, value)
     object[key] = locked;
     StorageArea.set(object);
 }
+
 function get(key)
 {
     StorageArea.get(key, function(object)
@@ -25,8 +20,13 @@ function get(key)
 function encrypt(value)
 {
     var secret = "";
-    secret.append()
-    return value;
+    for (var letter = 0; letter < value.length; letter++);
+    {
+        secret.concat(cipher[value.charAt(letter)]);
+    }
+    console.log(value);
+    console.log(secret);
+    return secret;
 }
 
 function decrypt(value)
@@ -34,8 +34,8 @@ function decrypt(value)
     return value;
 }
 StorageArea.clear(function(){});
-// store("one", "please save this, thank you");
-// get("one");
+store("one", "please save this, thank you");
+get("one");
 
 
 
@@ -56,3 +56,51 @@ StorageArea.clear(function(){});
 // {
 //     background.console.log(info.url);
 // })
+
+
+
+function setupCipher()
+{
+    var cipher = {};
+    var codes = new Array();
+
+    var i = 33;
+    var letter;
+    while (i < 128)
+    {
+        letter = String.fromCharCode(i);
+        codes.push(letter);
+        i++;
+    }
+
+    var valx, valy, x, y;
+    while (codes.length > 1)
+    {
+        x = Math.floor(Math.random() * (codes.length - 1) + 1);
+        y = Math.floor(Math.random() * (codes.length - 1) + 1);
+        if (x == y)
+        {
+            continue;
+        }
+        valx = codes[x];
+        valy = codes[y];
+        cipher[valx] = valy;
+        cipher[valy] = valx;
+        codes.splice(x, 1);
+        if (x > y)
+        {
+            codes.splice(y, 1);
+        }
+        else
+        {
+            codes.splice(y - 1, 1);
+        }
+    }
+    cipher[codes[0]] = codes[0];
+
+    // Debugging
+    // console.log(cipher);
+    // console.log(codes.length);
+    // console.log(Object.keys(cipher).length);
+    return cipher;
+}
