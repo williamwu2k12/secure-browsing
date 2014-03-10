a7d2d5e9f405dae6f5c4dbfcbea81e2965261b8b
William Wu <williamwu.2k12@gmail.com>
2014-03-10 00:19:45 -0700

    removed fetch.js and view.js, only had a few lines; go back to commit history if you needed it

--- fetch.js
+++
@@ -1,3 +0,0 @@
-// chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-//   console.log(tabs[0].url);
-// });
--- view.js
+++
@@ -1,5 +0,0 @@
-  // function createTab() {
-  //           var linkTo = {url: "http://www.stackoverflow.com"};
-  //           chrome.tabs.create(linkTo);
-  //          	}
-  //          	
